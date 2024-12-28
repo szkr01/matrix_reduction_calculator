7879d37617c5fdef822a1cee8595fca9aa0fc696
korag <suzuki82210@gmail.com>
2024-12-29 04:23:24 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+	base: process.env.GITHUB_PAGES // この行を追加
+		? "reactailwind" // この行を追加
+		: "./", // この行を追加
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
 });
